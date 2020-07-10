eec318fcfb02bd2f14270633c7a108fb03ce9c84
peet-j <James.Peet@amido.com>
2020-07-10 17:21:53 +0100

    Added Test Tags where missing and also PR Template

--- java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/v1/menu/dto/SearchMenuResultItemTest.java
+++ java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/v1/menu/dto/SearchMenuResultItemTest.java
@@ -1,14 +1,14 @@
 package com.xxAMIDOxx.xxSTACKSxx.api.v1.menu.dto;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Tag("Unit")
 class SearchMenuResultItemTest {
 
   @Test
   void testEquals() {
-    EqualsVerifier.simple().forClass(SearchMenuResult.class).verify();
+    EqualsVerifier.simple().forClass(SearchMenuResultItem.class).verify();
   }
 }
