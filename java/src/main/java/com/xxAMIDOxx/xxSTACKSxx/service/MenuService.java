86e9692523534cd0edf21f24fb1e0a527b8aa774
Suresh Krishnan <suresh.krishnan@amido.com>
2020-07-10 19:00:42 +0100

    task-2300- search by filter terms

--- java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/service/MenuService.java
+++ java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/service/MenuService.java
@@ -1,6 +1,8 @@
 package com.xxAMIDOxx.xxSTACKSxx.service;
 
 import com.xxAMIDOxx.xxSTACKSxx.model.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +10,41 @@ import java.util.UUID;
 
 public interface MenuService {
 
+    /**
+     * @param pageNumber
+     * @param pageSize
+     * @return
+     */
     List<Menu> all(int pageNumber, int pageSize);
 
+    /**
+     * @param id
+     * @return
+     */
     Optional<Menu> findById(UUID id);
+
+    /**
+     * @param restaurantId
+     * @param pageable
+     * @return
+     */
+    Page<Menu> findAllByRestaurantId(UUID restaurantId, Pageable pageable);
+
+
+    /**
+     * @param searchTerm
+     * @param pageable
+     * @return
+     */
+    Page<Menu> findAllByNameContaining(String searchTerm, Pageable pageable);
+
+    /**
+     * @param restaurantId
+     * @param searchTerm
+     * @param pageable
+     * @return
+     */
+    Page<Menu> findAllByRestaurantIdAndNameContaining(UUID restaurantId, String searchTerm, Pageable pageable);
 }
+
+
--- java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/service/impl/MenuServiceImpl.java
+++ java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/service/impl/MenuServiceImpl.java
@@ -1,22 +1,32 @@
 package com.xxAMIDOxx.xxSTACKSxx.service.impl;
 
+import com.microsoft.azure.spring.data.cosmosdb.core.CosmosTemplate;
 import com.microsoft.azure.spring.data.cosmosdb.core.query.CosmosPageRequest;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.Criteria;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.CriteriaType;
+import com.microsoft.azure.spring.data.cosmosdb.core.query.DocumentQuery;
 import com.xxAMIDOxx.xxSTACKSxx.model.Menu;
 import com.xxAMIDOxx.xxSTACKSxx.repository.MenuRepository;
 import com.xxAMIDOxx.xxSTACKSxx.service.MenuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@SuppressWarnings("checkstyle:Indentation")
 @Service
 public class MenuServiceImpl implements MenuService {
+
+    @Autowired
+    private CosmosTemplate cosmosTemplate;
 
     private static Logger logger = LoggerFactory.getLogger(MenuServiceImpl.class);
 
@@ -48,5 +58,32 @@ public class MenuServiceImpl implements MenuService {
 
     public Optional<Menu> findById(UUID id) {
         return this.menuRepository.findById(id.toString());
+    }
+
+    @Override
+    public Page<Menu> findAllByRestaurantId(UUID restaurantId, Pageable pageable) {
+        return this.menuRepository.findAllByRestaurantId(restaurantId,pageable);
+    }
+
+//    @Override
+//    public Page<Menu> findAllByNameContaining(String searchTerm, Pageable pageable) {
+//        return this.menuRepository.findAllByNameContaining(searchTerm, pageable);
+//    }
+
+
+    @Override
+    public Page<Menu> findAllByNameContaining(String searchTerm, Pageable pageable) {
+
+        DocumentQuery query = new DocumentQuery(Criteria.getInstance(CriteriaType.CONTAINING, "name",
+                Collections.singletonList(searchTerm))).with(pageable);
+        return cosmosTemplate.paginationQuery(query, Menu.class, cosmosTemplate.getContainerName(Menu.class));
+
+        //return this.menuRepository.findAllByNameContaining(searchTerm, pageable);
+    }
+
+
+    @Override
+    public Page<Menu> findAllByRestaurantIdAndNameContaining(UUID restaurantId, String searchTerm, Pageable pageable) {
+        return this.menuRepository.findAllByRestaurantIdAndNameContaining(restaurantId,searchTerm,pageable);
     }
 }
