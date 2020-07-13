2b057eb5561d6702349d503f824c66a0df476f36
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-13 11:27:55 +0100

    fix: Use new way of deploying infra

--- deploy/azure/app/kube/main.tf
+++ deploy/azure/app/kube/main.tf
@@ -10,59 +10,27 @@ module "default_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
   namespace  = "${var.name_company}-${var.name_project}"
   stage      = var.stage
-  name       = "${lookup(var.location_name_map, var.resource_group_location, "euw")}-${var.name_domain}"
+  name       = "${lookup(var.location_name_map, var.resource_group_location, "westeurope")}-${var.name_component}"
   attributes = var.attributes
   delimiter  = "-"
   tags       = var.tags
 }
 
-locals {
-  create_resource_group = var.use_existing_resource_group && var.resource_group_name != "" ? false : true
-  resource_group_name   = var.use_existing_resource_group && var.resource_group_name != "" ? var.resource_group_name : module.default_label.id
-}
-
-resource "azurerm_resource_group" "default" {
-  count    = local.create_resource_group ? 1 : 0
-  name     = local.resource_group_name
-  location = var.resource_group_location
-  tags     = var.tags
-}
-
-####
-# app level DNS can/should be controlled from here
-# an alternative way of managing this would be through K8s operators
-# [TODO]: examples can be found in the deploy folders
-####
-resource "azurerm_dns_a_record" "default" {
-  name                = var.dns_record
-  zone_name           = var.dns_zone_name
-  resource_group_name = local.resource_group_name
-  ttl                 = 300
-  records             = [data.azurerm_public_ip.app_gateway.ip_address]
-}
-
-module "cosmosdb" {
-  source                               = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-cosmosdb?ref=v1.1.0"
+module "app" {
+  source                               = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-server-side-app?ref=v1.3.0"
   create_cosmosdb                      = var.create_cosmosdb
   resource_namer                       = module.default_label.id
-  name_environment                     = "dev-feature"
-  name_project                         = var.name_project
-  name_company                         = var.name_company
-  name_component                       = var.name_domain
-  resource_group_name                  = local.resource_group_name
-  cosmosdb_sql_container               = "Menu"
-  cosmosdb_sql_container_partition_key = "/id"
-  cosmosdb_kind                        = "GlobalDocumentDB"
-  cosmosdb_offer_type                  = "Standard"
+  resource_tags                        = module.default_label.tags
+  cosmosdb_sql_container               = var.cosmosdb_sql_container
+  cosmosdb_sql_container_partition_key = var.cosmosdb_sql_container_partition_key
+  cosmosdb_kind                        = var.cosmosdb_kind
+  cosmosdb_offer_type                  = var.cosmosdb_offer_type
+  create_cache                         = var.create_cache
+  create_dns_record                    = true
+  dns_record                           = var.dns_record
+  dns_zone_name                        = var.dns_zone_name
+  dns_zone_resource_group              = var.dns_zone_resource_group != "" ? var.dns_zone_resource_group : var.core_resource_group
+  dns_a_records                        = [data.azurerm_public_ip.app_gateway.ip_address]
+  # Alternatively if you want you can pass in the IP directly and remove the need for a lookup
+  # dns_a_records                        = ["0.1.23.45"]
 }
-
-####
-# Additional modules need to go here as they can be re-used across app deployments
-####
-# module "observability" {
-#   source = "git://...."
-# }
-
-####
-# Additional user defined resources or modules can go here
-####
