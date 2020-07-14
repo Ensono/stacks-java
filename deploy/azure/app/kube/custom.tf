4daf5ebf24f676ca9c1e972df679b0c060c9bd0d
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-14 10:00:10 +0100

    fix: tf linting

--- deploy/azure/app/kube/custom.tf
+++ deploy/azure/app/kube/custom.tf
@@ -1,4 +1,4 @@
-# Example of further extensions to STacks Core templates
+# Example of further extensions to Stacks Core templates
 # Potential user defined extensions
 data "azurerm_application_insights" "example" {
   name                = var.core_resource_group
@@ -7,12 +7,12 @@ data "azurerm_application_insights" "example" {
 
 output "app_insights_instrumentation_key" {
   description = "App Insights key for downstream deploymnent use"
-  value = data.azurerm_application_insights.example.instrumentation_key
-  sensitive = true
+  value       = data.azurerm_application_insights.example.instrumentation_key
+  sensitive   = true
 }
 
 variable app_insights_name {
-  type = string
-  default = ""
+  type        = string
+  default     = ""
   description = "app insights name for key retriaval in memory"
 }
