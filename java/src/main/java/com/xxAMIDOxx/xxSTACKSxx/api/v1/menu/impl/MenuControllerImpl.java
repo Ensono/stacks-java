db7510e8e720ee72f1b0b8634c2482e18e4872d4
Suresh Krishnan <suresh.krishnan@amido.com>
2020-07-14 11:14:24 +0100

    task-2300- search by filter terms

--- java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/api/v1/menu/impl/MenuControllerImpl.java
+++ java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/api/v1/menu/impl/MenuControllerImpl.java
@@ -33,23 +33,19 @@ public class MenuControllerImpl implements MenuController {
                                                        final Integer pageSize,
                                                        final Integer pageNumber) {
 
+        SearchMenuResult searchMenuResult;
+
         if (StringUtils.isNotEmpty(searchTerm) && Objects.nonNull(restaurantId)) {
-            return ResponseEntity.ok(
-                    new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByRestaurantIdAndNameContaining(restaurantId, searchTerm, pageSize, pageNumber)));
+            searchMenuResult = new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByRestaurantIdAndNameContaining(restaurantId, searchTerm, pageSize, pageNumber));
+        } else if (StringUtils.isNotEmpty(searchTerm)) {
+            searchMenuResult = new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByNameContaining(searchTerm, pageSize, pageNumber));
+        } else if (Objects.nonNull(restaurantId)) {
+            searchMenuResult = new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByRestaurantId(restaurantId, pageSize, pageNumber));
+        } else {
+            searchMenuResult = new SearchMenuResult(pageSize, pageNumber, this.menuService.all(pageNumber, pageSize));
         }
 
-        if (StringUtils.isNotEmpty(searchTerm)) {
-            return ResponseEntity.ok(
-                    new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByNameContaining(searchTerm, pageSize, pageNumber)));
-        }
-
-        if (Objects.nonNull(restaurantId)) {
-            return ResponseEntity.ok(
-                    new SearchMenuResult(pageSize, pageNumber, this.menuService.findAllByRestaurantId(restaurantId, pageSize, pageNumber)));
-        }
-
-        return ResponseEntity.ok(
-                new SearchMenuResult(pageSize, pageNumber, this.menuService.all(pageNumber, pageSize)));
+        return ResponseEntity.ok(searchMenuResult);
     }
 
 
