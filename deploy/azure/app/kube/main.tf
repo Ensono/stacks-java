4c1c2ba156e4370e7b2cdb3dfe987f9f4b7e50e6
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-14 20:25:12 +0100

    fix: rolback

--- deploy/azure/app/kube/main.tf
+++ deploy/azure/app/kube/main.tf
@@ -17,7 +17,7 @@ module "default_label" {
 }
 
 module "app" {
-  source                               = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-server-side-app?ref=fix/cosmos-outputs"
+  source                               = "git::https://github.com/amido/stacks-terraform//azurerm/modules/azurerm-server-side-app?ref=v1.3.1"
   create_cdn_endpoint                  = var.create_cdn_endpoint
   create_cosmosdb                      = var.create_cosmosdb
   resource_namer                       = module.default_label.id
