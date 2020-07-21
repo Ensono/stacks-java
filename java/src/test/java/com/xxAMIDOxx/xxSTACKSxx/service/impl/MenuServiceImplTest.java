616c74269b910f2d1962f5b646b050e68901bda0
Akrish17 <67281032+Akrish17@users.noreply.github.com>
2020-07-21 09:34:23 +0100

    Feature/2261- Create Menu endpoint (#24)

--- java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/service/impl/MenuServiceImplTest.java
+++ java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/service/impl/MenuServiceImplTest.java
@@ -3,46 +3,81 @@ package com.xxAMIDOxx.xxSTACKSxx.service.impl;
 import com.xxAMIDOxx.xxSTACKSxx.model.Menu;
 import com.xxAMIDOxx.xxSTACKSxx.repository.MenuRepository;
 import com.xxAMIDOxx.xxSTACKSxx.service.MenuService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.xxAMIDOxx.xxSTACKSxx.model.MenuHelper.createMenus;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @Tag("Unit")
 public class MenuServiceImplTest {
 
-    @Test
-    void findAll() {
+  @Mock
+  MenuRepository repository;
 
-        // Given
-        MenuRepository repository = mock(MenuRepository.class);
-        MenuService menuServiceImpl = new MenuServiceImpl(repository);
+  @Mock
+  Menu menu;
 
-        List<Menu> results = createMenus(2);
-        Page<Menu> page1 = mock(Page.class);
-        Page<Menu> page2 = new PageImpl<>(results);
-        Pageable pageable = mock(Pageable.class);
+  @InjectMocks
+  MenuServiceImpl menuServiceImpl;
 
-        when(repository.findAll(any(Pageable.class))).thenReturn(page1);
-        when(page1.hasNext()).thenReturn(true);
-        when(page1.nextPageable()).thenReturn(pageable);
-        when(repository.findAll(eq(pageable))).thenReturn(page2);
+  @BeforeEach
+  void init_mocks() {
+    MockitoAnnotations.initMocks(this);
+    menu = new Menu();
+    menu.setId(UUID.randomUUID().toString());
+    menu.setEnabled(true);
+    menu.setName("testMenu");
+    menu.setRestaurantId(UUID.randomUUID());
+    menu.setDescription("something");
+  }
 
-        // When
-        List<Menu> actualResults = menuServiceImpl.findAll(2, 5);
+  @Test
+  void findAll() {
 
-        // Then
-        then(actualResults).isEqualTo(results);
+    // Given
+    MenuRepository repository = mock(MenuRepository.class);
+    MenuService menuServiceImpl = new MenuServiceImpl(repository);
 
-    }
+    List<Menu> results = createMenus(2);
+    Page<Menu> page1 = mock(Page.class);
+    Page<Menu> page2 = new PageImpl<>(results);
+    Pageable pageable = mock(Pageable.class);
+
+    when(repository.findAll(any(Pageable.class))).thenReturn(page1);
+    when(page1.hasNext()).thenReturn(true);
+    when(page1.nextPageable()).thenReturn(pageable);
+    when(repository.findAll(eq(pageable))).thenReturn(page2);
+
+    // When
+    List<Menu> actualResults = menuServiceImpl.findAll(2, 5);
+
+    // Then
+    then(actualResults).isEqualTo(results);
+  }
+
+  @Test
+  void testSaveMenu() {
+    // Given
+    when(repository.save(any(Menu.class))).thenReturn(menu);
+    // When
+    Menu actualResults = menuServiceImpl.saveMenu(menu);
+    // Then
+    then(actualResults).isSameAs(menu);
+    then(actualResults.getId()).isNotEmpty();
+  }
 }
