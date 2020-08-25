d5721f64c424735c3cefa24526c9b81cfd95c512
Suresh Krishnan <suresh.krishnan@amido.com>
2020-08-25 15:46:03 +0100

    feature-2364 : Auth0 implementation config switch

--- java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
+++ java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/Application.java
@@ -3,10 +3,9 @@ package com.xxAMIDOxx.xxSTACKSxx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySources({@PropertySource("classpath:auth0.properties")})
+@PropertySource("classpath:auth0.properties")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
