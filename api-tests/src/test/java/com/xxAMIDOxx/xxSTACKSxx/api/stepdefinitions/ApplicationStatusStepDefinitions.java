320fa8e6fff23ccb6491bbfbc1836a1f12af91ee
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-09-01 10:22:01 +0100

    feat: Adds Checkstyle, fmt, and spotbugs to build

--- api-tests/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/stepdefinitions/ApplicationStatusStepDefinitions.java
+++ api-tests/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/stepdefinitions/ApplicationStatusStepDefinitions.java
@@ -1,5 +1,9 @@
 package com.xxAMIDOxx.xxSTACKSxx.api.stepdefinitions;
 
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 import com.xxAMIDOxx.xxSTACKSxx.api.status.AppStatus;
 import com.xxAMIDOxx.xxSTACKSxx.api.status.ApplicationStatus;
 import io.cucumber.java.en.Given;
@@ -8,32 +12,28 @@ import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import org.hamcrest.Matchers;
 
-import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
 public class ApplicationStatusStepDefinitions {
 
-    @Steps
-    ApplicationStatus theApplication;
+  @Steps ApplicationStatus theApplication;
 
-    @Given("the application is running")
-    public void the_application_is_running() {
-        assertThat(theApplication.currentStatus()).isEqualTo(AppStatus.RUNNING);
-    }
+  @Given("the application is running")
+  public void the_application_is_running() {
+    assertThat(theApplication.currentStatus()).isEqualTo(AppStatus.RUNNING);
+  }
 
-    @When("I check the application status")
-    public void check_the_application_status() {
-        theApplication.readStatusMessage();
-    }
+  @When("I check the application status")
+  public void check_the_application_status() {
+    theApplication.readStatusMessage();
+  }
 
-    @Then("the API should return {string}")
-    public void the_API_should_return(String expectedMessage) {
-        restAssuredThat(lastResponse -> lastResponse.body(equalTo(expectedMessage)));
-    }
+  @Then("the API should return {string}")
+  public void the_API_should_return(String expectedMessage) {
+    restAssuredThat(lastResponse -> lastResponse.body(equalTo(expectedMessage)));
+  }
 
-    @Then("the API should return {string} status")
-    public void the_API_return_status(String status) {
-        restAssuredThat (lastResponse -> lastResponse.body("status", Matchers.equalToIgnoringCase(status)));
-    }
+  @Then("the API should return {string} status")
+  public void the_API_return_status(String status) {
+    restAssuredThat(
+        lastResponse -> lastResponse.body("status", Matchers.equalToIgnoringCase(status)));
+  }
 }
