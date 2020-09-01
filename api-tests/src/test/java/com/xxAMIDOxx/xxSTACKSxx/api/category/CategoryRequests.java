baedaedf876f1d02bf4b6244072bb0dc021b4c85
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-09-01 10:22:01 +0100

    feat: Adds Checkstyle, fmt, and spotbugs to build

--- api-tests/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/category/CategoryRequests.java
+++ api-tests/src/test/java/com/xxAMIDOxx/xxSTACKSxx/api/category/CategoryRequests.java
@@ -6,36 +6,45 @@ import net.thucydides.core.annotations.Step;
 
 public class CategoryRequests {
 
-    String menuUrl = WebServiceEndPoints.BASE_URL.getUrl() + WebServiceEndPoints.MENU.getUrl();
+  String menuUrl = WebServiceEndPoints.BASE_URL.getUrl() + WebServiceEndPoints.MENU.getUrl();
 
-    @Step("Create a new category")
-    public void createCategory(String body, String menuID) {
-        SerenityRest.given()
-                .contentType("application/json")
-                .header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .post(menuUrl.concat("/").concat(menuID).concat(WebServiceEndPoints.CATEGORY.getUrl()));
-    }
+  @Step("Create a new category")
+  public void createCategory(String body, String menuID) {
+    SerenityRest.given()
+        .contentType("application/json")
+        .header("Content-Type", "application/json")
+        .body(body)
+        .when()
+        .post(menuUrl.concat("/").concat(menuID).concat(WebServiceEndPoints.CATEGORY.getUrl()));
+  }
 
-    @Step("Update the category")
-    public void updateCategory(String body, String menuID, String categoryID) {
-        SerenityRest.given()
-                .contentType("application/json")
-                .header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .put(menuUrl.concat("/").concat(menuID).concat(WebServiceEndPoints.CATEGORY.getUrl())
-                        .concat("/").concat(categoryID));
-    }
+  @Step("Update the category")
+  public void updateCategory(String body, String menuID, String categoryID) {
+    SerenityRest.given()
+        .contentType("application/json")
+        .header("Content-Type", "application/json")
+        .body(body)
+        .when()
+        .put(
+            menuUrl
+                .concat("/")
+                .concat(menuID)
+                .concat(WebServiceEndPoints.CATEGORY.getUrl())
+                .concat("/")
+                .concat(categoryID));
+  }
 
-    @Step("Delete the category")
-    public void deleteTheCategory(String menuID, String categoryID) {
-        SerenityRest.given()
-                .contentType("application/json")
-                .when()
-                .delete(menuUrl.concat("/").concat(menuID)
-                        .concat(WebServiceEndPoints.CATEGORY.getUrl())
-                        .concat("/").concat(categoryID));
-    }
+  @Step("Delete the category")
+  public void deleteTheCategory(String menuID, String categoryID) {
+    SerenityRest.given()
+        .contentType("application/json")
+        .when()
+        .delete(
+            menuUrl
+                .concat("/")
+                .concat(menuID)
+                .concat(WebServiceEndPoints.CATEGORY.getUrl())
+                .concat("/")
+                .concat(categoryID));
+  }
 }
