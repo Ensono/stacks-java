09c29dc422f8a2896fbf8a847b984224051a1caf
Suresh Krishnan <suresh.krishnan@amido.com>
2020-09-02 09:20:39 +0100

    feature-2364 : Auth0 swagger spec

--- java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/menu/api/v1/impl/AuthControllerImplTest.java
+++ java/src/test/java/com/xxAMIDOxx/xxSTACKSxx/menu/api/v1/impl/AuthControllerImplTest.java
@@ -29,18 +29,6 @@ class AuthControllerImplTest {
 
   @Autowired private TestRestTemplate testRestTemplate;
 
-  @Value(value = "${auth0.client_id}")
-  private String clientId;
-
-  @Value(value = "${auth0.client_secret}")
-  private String clientSecret;
-
-  @Value(value = "${auth0.apiAudience}")
-  private String apiAudience;
-
-  @Value(value = "${auth0.grant_type}")
-  private String grantType;
-
   @Test
   void testGenerateTokenWhenInvalidCredentials() {
     // Given
@@ -61,28 +49,5 @@ class AuthControllerImplTest {
 
     // Then
     then(response.getStatusCode()).isEqualTo(INTERNAL_SERVER_ERROR);
-  }
-
-
-  @Test
-  void testGenerateTokenWhenValidCredentials() {
-    // Given
-    GenerateTokenRequest requestBody = new GenerateTokenRequest();
-    requestBody.setClient_id(clientId);
-    requestBody.setClient_secret(clientSecret);
-    requestBody.setAudience(apiAudience);
-    requestBody.setGrant_type(grantType);
-
-    // When
-    String requestUrl = String.format(GENERATE_TOKEN_URI, getBaseURL(port));
-    var response =
-            this.testRestTemplate.exchange(
-                    requestUrl,
-                    HttpMethod.POST,
-                    new HttpEntity<>(requestBody, getRequestHttpEntity()),
-                    GenerateTokenResponse.class);
-
-    // Then
-    then(response.getStatusCode()).isEqualTo(OK);
   }
 }
