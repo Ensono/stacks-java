c04dcd8f33c09112f1c22d58c47d98d54e060905
Steve Clewer <steve.clewer@amido.com>
2020-10-12 22:03:01 +0100

    Removed UpdateCategory command/handler

--- java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/menu/exception/CategoryDoesNotExistException.java
+++ java/src/main/java/com/xxAMIDOxx/xxSTACKSxx/menu/exception/CategoryDoesNotExistException.java
@@ -1,6 +1,8 @@
 package com.xxAMIDOxx.xxSTACKSxx.menu.exception;
 
 import com.xxAMIDOxx.xxSTACKSxx.menu.commands.MenuCommand;
+import com.xxAMIDOxx.xxSTACKSxx.menu.commands.OperationCode;
+
 import java.util.UUID;
 
 public class CategoryDoesNotExistException extends MenuApiException {
@@ -11,5 +13,14 @@ public class CategoryDoesNotExistException extends MenuApiException {
             categoryId, command.getMenuId()),
         ExceptionCode.CATEGORY_DOES_NOT_EXIST,
         command);
+  }
+
+  public CategoryDoesNotExistException(String categoryId, String menuId, int operationCode, String correlationId) {
+    super(
+            String.format(
+                    "A category with the id '%s' does not exist for menu with id '%s'.",
+                    categoryId, menuId),
+            ExceptionCode.CATEGORY_DOES_NOT_EXIST,
+            operationCode, correlationId);
   }
 }
