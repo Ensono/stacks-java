7db6665cf519169adc382afcaf7315a03ffec03b
Jack Blower <Jack@elvenspellmaker.co.uk>
2021-06-10 20:18:39 +0100

    fix: Fixes various npm audit and test 4 dependabot

--- deploy/azure/app/kube/constraints.tf
+++ deploy/azure/app/kube/constraints.tf
@@ -1,8 +1,17 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    azurerm = "~> 2.5"
-    null    = "~> 2.1"
-    tls     = "~> 2.1"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.5"
+    }
+    null    = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    tls     = {
+      source  = "hashicorp/tls"
+      version = "~> 2.1"
+    }
   }
 }
