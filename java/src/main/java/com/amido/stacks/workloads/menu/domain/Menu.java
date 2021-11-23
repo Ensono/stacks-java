9ee4b877992beb1685f74acd4bb3aa7652813c58
guillermo <guillonrg@gmail.com>
2021-11-23 11:03:37 +0000

    Refactor for the modularity switch

---
+++ java/src/main/java/com/amido/stacks/workloads/menu/domain/Menu.java
@@ -0,0 +1,39 @@
+package com.amido.stacks.workloads.menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class Menu {
+
+  @Id private String id;
+
+  private String restaurantId;
+
+  private String name;
+
+  private String description;
+
+  @Builder.Default private List<Category> categories = new ArrayList<>();
+
+  private Boolean enabled;
+
+  public Menu addOrUpdateCategory(Category category) {
+    if (this.categories == null) {
+      this.categories = new ArrayList<>();
+    }
+    this.categories =
+        this.categories.stream()
+            .filter(c -> !c.getId().equals(category.getId()))
+            .collect(Collectors.toList());
+    this.categories.add(category);
+    return this;
+  }
+}
