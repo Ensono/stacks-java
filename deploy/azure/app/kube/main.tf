6f3bb49f2ca6c9231e6102f57fecfe49e01d877d
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-08-12 23:40:22 +0100

    fix: fix for var name change

--- deploy/azure/app/kube/main.tf
+++ deploy/azure/app/kube/main.tf
@@ -33,8 +33,8 @@ module "app" {
   dns_record                           = var.dns_record
   dns_zone_name                        = var.dns_zone_name
   infra_resource_group                 = var.infra_resource_group
-  dns_zone_resource_group              = var.dns_zone_resource_group != "" ? var.dns_zone_resource_group : var.core_resource_group
+  dns_zone_resource_group              = var.dns_zone_resource_group != "" ? var.dns_zone_resource_group : var.infra_resource_group
   dns_ip_address_name                  = var.app_gateway_frontend_ip_name
-  dns_ip_address_resource_group        = var.core_resource_group
+  dns_ip_address_resource_group        = var.infra_resource_group
   subscription_id                      = data.azurerm_client_config.current.subscription_id
 }
