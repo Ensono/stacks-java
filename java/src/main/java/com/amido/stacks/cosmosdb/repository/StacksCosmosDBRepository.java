5468006f126145f7a4e678beddd4f10d087a3377
ShaunMathias <shaun.mathias@ensono.com>
2024-12-10 10:53:35 +0000

    Added the cosmos and dynamo main db code and folders (#2)
    * added the cosmos and dynamo main db code and folders
    
    * added comments for placeholder classes and interfaces
    
    * corrected comment

---
+++ java/src/main/java/com/amido/stacks/cosmosdb/repository/StacksCosmosDBRepository.java
@@ -0,0 +1,20 @@
+package com.amido.stacks.cosmosdb.repository;
+
+#if USE_COSMOSDB
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
+import org.springframework.data.repository.CrudRepository;
+#endif
+import com.amido.stacks.core.repository.StacksPersistence;
+import org.springframework.data.repository.NoRepositoryBean;
+
+#if USE_COSMOSDB
+@NoRepositoryBean
+public interface StacksCosmosDBRepository<T>
+        extends StacksPersistence<T>, CrudRepository<T, String>, CosmosRepository<T, String> {}
+#else
+// Placeholder repository interface to ensure Maven compilation when Cosmos is not selected.
+// The entire Cosmos folder, including this file, is filtered out with project-builder-config.
+@NoRepositoryBean
+public interface StacksCosmosDBRepository<T> extends StacksPersistence<T> {
+}
+#endif
---
+++ java/src/main/java/com/amido/stacks/dynamodb/config/DynamoDBConfig.java
@@ -0,0 +1,50 @@
+package com.amido.stacks.dynamodb.config;
+
+#if USE_DYNAMODB
+import static com.amazonaws.util.StringUtils.isNullOrEmpty;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+#endif
+
+import org.springframework.context.annotation.Configuration;
+
+#if USE_DYNAMODB
+@Configuration
+@ConfigurationProperties(prefix = "amazon.dynamodb")
+@EnableDynamoDBRepositories(basePackages = "com.amido.stacks.workloads.menu.repository")
+public class DynamoDBConfig {
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String dynamoDbEndpoint;
+
+    @Value("${amazon.dynamodb.signingRegion}")
+    private String signingRegion;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+
+        AmazonDynamoDBClientBuilder clientBuilder =
+                AmazonDynamoDBClientBuilder.standard()
+                        .withCredentials(new DefaultAWSCredentialsProviderChain());
+
+        if (!isNullOrEmpty(dynamoDbEndpoint)) {
+            clientBuilder.withEndpointConfiguration(
+                    new EndpointConfiguration(dynamoDbEndpoint, signingRegion));
+        }
+
+        return clientBuilder.build();
+    }
+}
+#else
+// Placeholder configuration class to ensure Maven compilation when Dynamo is not selected.
+// The entire Dynamo folder, including this file, is filtered out with project-builder-config.
+@Configuration
+public class DynamoDBConfig {}
+#endif
