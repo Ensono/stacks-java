7592ca48307d9311043e03f43abeb8b39dedbe1c
Richard Slater <richard.slater@ensono.com>
2025-12-12 12:50:04 +0000

    fix: formatting

--- api-tests/src/test/java/com/amido/stacks/tests/api/UntaggedTestCleanupListener.java
+++ api-tests/src/test/java/com/amido/stacks/tests/api/UntaggedTestCleanupListener.java
@@ -4,7 +4,9 @@ import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
-/** Registers the shutdown hook that clears the transient report directories if no tests are found. */
+/**
+ * Registers the shutdown hook that clears the transient report directories if no tests are found.
+ */
 public final class UntaggedTestCleanupListener implements TestExecutionListener {
 
   @Override
