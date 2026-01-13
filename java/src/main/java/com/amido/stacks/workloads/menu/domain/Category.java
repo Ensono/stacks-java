50322e9dcbda0203aeb2901afc099e9269dba69e
Richard Slater <richard.slater@ensono.com>
2026-01-13 12:17:31 +0000

    chore: downgrade Spring Cloud to 2025.0.0; document reason in java/pom.xml; restore tests

--- java/src/main/java/com/amido/stacks/workloads/menu/domain/Category.java
+++ java/src/main/java/com/amido/stacks/workloads/menu/domain/Category.java
@@ -3,12 +3,10 @@ package com.amido.stacks.workloads.menu.domain;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-@Builder
 public class Category {
 
   private String id;
@@ -17,5 +15,5 @@ public class Category {
 
   private String description;
 
-  @Builder.Default private List<Item> items = new ArrayList<>();
+  private List<Item> items = new ArrayList<>();
 }
